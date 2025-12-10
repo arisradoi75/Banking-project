e44109dbfb81a22b0779dde66a59c711b009d732
bgd02 <bogdanbosoanca@gmail.com>
2025-12-10 16:45:18 +0200

    Repository pentru tranzactii

---
+++ src/main/java/com/exemple/bankingproject/repository/TransactionRepository.java
@@ -0,0 +1,8 @@
+package com.exemple.bankingproject.repository;
+
+import jakarta.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
