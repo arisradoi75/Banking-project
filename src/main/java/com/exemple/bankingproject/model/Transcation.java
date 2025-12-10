1ce89dfbf1987c3199dea73d603c16d640169840
arisradoi <radoiaris14@gmail.com>
2025-12-10 16:26:08 +0200

    first commit

---
+++ src/main/java/com/exemple/bankingproject/model/Transcation.java
@@ -0,0 +1,75 @@
+package com.exemple.bankingproject.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Transactions")
+public class Transcation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private Type type;
+    @Column(nullable = false)
+    private BigDecimal amount;
+    @Column
+    private String notes;
+    @Column(nullable = false)
+    private LocalDateTime dateTime;
+
+    public Transcation() {
+    }
+
+    public Transcation(Long id, Type type, BigDecimal amount, String notes, LocalDateTime dateTime) {
+        this.id = id;
+        this.type = type;
+        this.amount = amount;
+        this.notes = notes;
+        this.dateTime = dateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
